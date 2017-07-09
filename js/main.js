ace5dcfd596c0f7d97f57b23655569a236bb44ad
vrunda2712 <angy.v27@gmail.com>
2017-07-09 19:55:58 -0400

    Added click listener to the switch and able to on/off with message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-document.addEventListener('DOMContentLoaded',function(event) {
-  var status = document.querySelector('.status')
-  status.innerText = 'Document is ready!'
+document.addEventListener('DOMContentLoaded',function() {
+  var lightSwitch = document.querySelector('button');
+  var body = document.querySelector('body');
+
+  var message = document.querySelector('h1');
+
+  lightSwitch.addEventListener('click', function() {
+    if(body.classList.contains('light')) {
+      body.classList.remove('light');
+      body.classList.add('dark');
+      message.innerText = "Hey, who turned off the lights? ";
+
+      console.log('Hey, who turned off the lights?');
+    } else if(body.classList.contains('dark')) {
+      body.classList.remove('dark');
+      body.classList.add('light');
+      message.innerText = "It's so bright in here!";
+
+      console.log('It\'s so bright in here!');
+    };
+  });
 });
